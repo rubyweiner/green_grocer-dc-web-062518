4dd2f20458c932caec8994aeb13fad97f248f3d3
Ruby Weiner <rubyweiner796@me.com>
2018-06-19 22:07:06 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -48,5 +48,15 @@ cart.each do |item, price_hash|
 end
 
 def checkout(cart, coupons)
-  # code here
+ cart = consolidate_cart(items)
+  cart1 = apply_coupons(cart, coupons)
+  cart2 = apply_clearance(cart1)
+  
+  total = 0
+  
+  cart2.each do |name, price_hash|
+    total += price_hash[:price] * price_hash[:count]
+  end
+  
+  total > 100 ? total * 0.9 : total
 end
